1a7df5a601bfd5eaf2dc9fd0a6df7f5a21ec4047
Usman Ashraf <usmansiddiqi2000@gmail.com>
2025-12-29 22:51:57 +0530

    added new

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
 import Skills from "./components/Skills"
+import SplashCursor from "./components/SplashCursor"
 
 import WhatsAppWidget from './components/WhatsAppWidget'; 
 
@@ -15,6 +16,7 @@ function App() {
 
   return (
     <>
+    <SplashCursor />
 <Navbar/>
 <Hero/>
 <About/>
